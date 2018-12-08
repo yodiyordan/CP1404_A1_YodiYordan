bcd275ba5807da579c438d13ee2fa0a0f602ea85
yodisy <yodi.yordan@my.jcu.edu.au>
2018-12-08 23:47:30 +0800

    Valid check function and list song function added.

--- Songs To Learn 1.0 (Yodi Yordan).py
+++ Songs To Learn 1.0 (Yodi Yordan).py
@@ -4,7 +4,6 @@ Yodi Setiadi Yordan
 https://github.com/yodiyordan/CP1404_A1_YodiYordan/blob/master/Songs%20To%20Learn%201.0%20(Yodi%20Yordan).py
 """
 
-
 from operator import itemgetter
 
 data_sort = []
@@ -15,11 +14,15 @@ def main():
     open_file()
     print("Songs To Learn 1.0 - by Yodi Setiadi Yordan")
     total_songs = calculate_songs()
-    print("{} songs successfully loaded".format(total_songs))
+    print("{} songs loaded".format(total_songs))
     while True:
         print("Menu :")
         print("L - List songs\nA - Add new song\nC - Complete a song\nQ - Quit")
         answer = input(">>> ")
+        answer = answer.upper()
+        answer = valid_check(answer)
+        if answer == "L":
+            list_song()
 
 
 def open_file():
@@ -31,11 +34,35 @@ def open_file():
         data_sort.append(data)
     file.close()
 
+
 def calculate_songs():
     songs = 0
     for song in range(len(data_sort)):
         songs += 1
     return songs
+
+
+def valid_check(choice):
+    while choice != "L" and choice != "A" and choice != "C" and choice != "Q":
+        print("Invalid input, please enter a valid input (L, C, A or Q)")
+        print("Menu :")
+        print("L - List songs\nA - Add new song\nC - Complete a song\nQ - Quit")
+        choice = input(">>> ")
+        choice = choice.upper()
+    return choice
+
+
+def list_song():
+    songs_learned = 0
+    songs_not_learned = 0
+    for index, data in enumerate(data_sort):
+        if data[3] == "y":
+            print(index, "* {:30s} - {:25s} ({})".format(data[0], data[1], data[2]))
+            songs_not_learned += 1
+        else:
+            print(index, "  {:30s} - {:25s} ({})".format(data[0], data[1], data[2]))
+            songs_learned += 1
+    print("{} songs learned, {} songs still need to be learned".format(songs_learned, songs_not_learned))
 
 
 main()
